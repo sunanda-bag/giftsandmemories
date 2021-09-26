b72d5d0a6141aad327721481d00b37af16de6c83
Sunanda <sunandabag63@gmail.com>
2021-09-26 11:36:17 +0530

    step wizard functionality

--- bnt/static/assets/js/custom.js
+++ bnt/static/assets/js/custom.js
@@ -1,3 +1,134 @@
+if(localStorage.getItem("slide") === null){
+    restore_status();
+}
+var prod_list = {};
+var p_key=0;
+function step2(box_id){
+    if (typeof(Storage) !== "undefined") 
+    {
+        // Store
+        sessionStorage.setItem("box_id", box_id);
+        sessionStorage.setItem("slide",1); 
+        sessionStorage.setItem("max_val",1);
+    } 
+    else 
+    {
+        alert("Sorry, your browser does not support Session Storage...");
+    }
+
+    
+
+    var current = document.getElementById("step1");
+    var new_step = document.getElementById("step2");
+    current.className = "step0";
+    console.log("inside step 2");
+    new_step.className = "active step0";
+    console.log("inside step 3");
+        
+    var cards = document.getElementsByClassName("card1");
+    cards[0].className = "card1 b-0";
+    cards[1].className = "card1 b-0 show";
+}
+
+function step3(prod_id, qty){
+   
+    var p_list = {}
+   
+    p_list['product_id'] =prod_id;
+    p_list['quantity'] = qty;
+    prod_list[p_key] = p_list;
+    p_key=p_key+1;
+    
+    if (typeof(Storage) !== "undefined") 
+    {
+        // Store
+        sessionStorage.setItem("product_list", JSON.stringify(prod_list));
+      //  console.log(JSON.stringify(prod_list));
+        sessionStorage.setItem("slide",2); 
+        sessionStorage.setItem("max_val",2);
+        
+    } 
+    else 
+    {
+        alert("Sorry, your browser does not support Session Storage...");
+    }
+
+
+    $('#closemodal').click(function() {
+        $('#modalwindow').modal('hide');
+    });
+    $('.modal-backdrop').remove();
+}
+
+function instep(val,flag)
+{
+    var max_val=parseInt(sessionStorage.getItem("max_val"));
+    console.log("type of val: ",typeof(val),val);
+    console.log("type of maxval: ",typeof(max_val),max_val);
+    if(flag==1)
+    {
+        sessionStorage.setItem("max_val",val);
+    }
+    if(val<=max_val || flag==1)
+    {
+    console.log("before restore status");
+    sessionStorage.setItem("slide",val); 
+    restore_status();
+    }
+
+}
+
+function restore_status()
+{
+
+    var mem = sessionStorage.getItem("slide");
+    console.log("value of slide1: "+mem);
+    if(mem===null){
+        console.log("slide value is null");
+    }
+    else 
+    {
+     var data = JSON.parse(JSON.stringify(sessionStorage.product_list));
+     console.log("product list: " ,data);
+    console.log("value of slide: "+mem);
+    console.log("step"+mem);
+    //var current = document.getElementById("step"+mem);
+    var new_step = document.getElementById("step"+(parseInt(mem) +1));
+    //current.className = "step0";
+    console.log("inside restore status");
+   
+
+        
+    var cards = document.getElementsByClassName("card1");
+    for(var k=0; k<cards.length; k++)
+    {
+    $("#progressbar").children()[k].className ="step0";
+    cards[k].className = "card1 b-0";
+    }
+    new_step.className = "active step0";
+    cards[parseInt(mem)].className = "card1 b-0 show";
+    }
+}
+
+
+function step4(box_id){
+    sessionStorage.setItem("max_val",2);
+    sessionStorage.setItem("slide",3); 
+    var current = document.getElementById("step3");
+    var new_step = document.getElementById("step4");
+    current.className = "step0";
+    console.log("insie step 4");
+    new_step.className = "active step0";
+    console.log("insie step 5");
+        
+    var cards = document.getElementsByClassName("card1");
+    cards[2].className = "card1 b-0";
+    cards[3].className = "card1 b-0 show";
+}
+
+
+
+
 function validate1(val) {
     v1 = document.getElementById("name");
     v2 = document.getElementById("email");
